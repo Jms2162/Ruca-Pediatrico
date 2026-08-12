b304b56b228937383d707e59100dc02742a24171
Joaquin Sanguinetti <joacosangui@gmail.com>
2026-08-12 11:25:44 -0300

    Primer cambio so2bre script main y sus fotos

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,27 @@
 const servicios = [
   {
     titulo: 'Consultas Pediátricas Generales',
-    imagen: '/images/consulta-pediatrica.jpg',
+    imagen: 'images/consulta-pediatrica.jpg',
     descripcion: 'Evaluaciones de salud, diagnóstico y tratamiento de enfermedades, seguimiento del crecimiento y desarrollo.'
   },
   {
     titulo: 'Atención pediátrica fines de semana',
-    imagen: '/images/guardia-medica.jpg',
+    imagen: 'images/guardia-medica.jpg',
     descripcion: 'Contamos con una guardia pediátrica pasiva todos los fines de semana y feriados de 9 a 20 hs para dar respuesta a problemas de salud inesperados de forma ágil y personalizada. Consulta por nuestra afiliación (para agendar un turno llamanos a nuestro Whatsapp) <a href="https://wa.me/5492954447555" target="_blank">Turnos por Whatsapp</a>'
   },
   {
     titulo: 'Consultorio de alergia',
-    imagen: '/images/alergia-e-inmunologia.jpeg',
+    imagen: 'images/alergia-e-inmunologia.jpeg',
     descripcion: 'Diagnóstico y tratamientos de enfermedades alérgicas. Identificar la causa de tu alergia es la mejor manera de controlarla.'
   },
   {
     titulo: 'Asesoramiento Nutricional',
-    imagen: '/images/foto-nutricion.jpeg',
+    imagen: 'images/foto-nutricion.jpeg',
     descripcion: 'Con nuestro equipo de nutricionistas brindamos la mejor información sobre nutrición integral, alimentación complementaria y BLW (Baby-Led weaning).'
   },
   {
     titulo: 'Talleres',
-    imagen: '/images/talleres-medicos.jpg',
+    imagen: 'images/talleres-medicos.jpg',
     descripcion: 'Sesiones informativas y talleres para la comunidad: RCP, Uso del DEA, Alimentación complementaria, Sueño, entre otros.'
   }
 ];
@@ -35,7 +35,7 @@ function renderizarServicios() {
   const encabezado = document.createElement('div');
   const imagenEncabezado = document.createElement('img');
   imagenEncabezado.className = 'imagen-servicios';
-  imagenEncabezado.src = '/images/foto-servicios.png';
+  imagenEncabezado.src = 'images/foto-servicios.png';
   imagenEncabezado.alt = 'Nuestros Servicios';
   const tituloSeccion = document.createElement('h3');
   tituloSeccion.textContent = 'Nuestros Servicios';
